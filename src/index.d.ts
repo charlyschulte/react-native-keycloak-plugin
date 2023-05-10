a9db59e60f1e1f8616aa7130cecd52a7977331b8
charlyschulte <dddbernhard@yahoo.de>
2023-05-10 20:55:44 +0200

    added typescript

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,16 +1,18 @@
+import { UserCredentials } from "react-native-keychain";
+
 export interface keycloakConfig {
-    realm: string;
-    appsiteUri: string;
-    'auth-server-url': string;
-    'ssl-required': string;
-    redirectUri: string;
-    resource: string;
-    credentials: {
-      secret: string;
-    };
-    'confidential-port': number;
+  realm: string;
+  appsiteUri: string;
+  'auth-server-url': string;
+  'ssl-required': string;
+  redirectUri: string;
+  resource: string;
+  credentials: {
+    secret: string;
   };
-  export interface storedToken {
+  'confidential-port': number;
+}
+export interface storedToken {
   access_token: string;
   expires_in: string;
   refresh_expires_in: string;
@@ -19,8 +21,59 @@ export interface keycloakConfig {
   session_state: string;
   token_type: string;
 }
+export interface scope {
+  scope:string
+}
+export interface loginOptions{
+  storeInfo: boolean;
+  scope: string;
+}
+export interface refreshLoginOptions {
+  scope: string;
+  inputConf: {
+
+  };
+  inputCredentials: {
+    username : string;
+    password : string;
+    storeInfo: boolean;
+  }
+}
+export interface retrieveUserInfoReturn{
+  email: string; 
+  email_verified: boolean;
+  preferred_username: string;
+  sub: string;
+}
+export interface logoutInterface{
+  destroySession: boolean;
+  inputConf: JSON;
+  inputTokens:JSON
+}
+export interface refreshTokenInterface{
+  inputConf: JSON;
+  inputTokens: JSON;
+}
 declare module 'TokenStorage' {
-    async function saveCredentials(username:string,password:string):Promise<void>;
-    async function saveConfiguration(conf:keycloakConfig):Promise<void>;
-    async function saveTokens(tokens:storedToken):Promise<void>;
+  function saveCredentials(username: string, password: string): Promise<void>;
+  function saveConfiguration(conf: keycloakConfig): Promise<void>;
+  function saveTokens(tokens: storedToken): Promise<void>;
+  function getCredentials(): Promise<false | UserCredentials>;
+  function getConfiguration(): Promise<undefined | keycloakConfig>;
+  function getTokens(): Promise<undefined | storedToken>;
+  function clearSession(): Promise<void>
+}
+declare module 'TokensUtils' {
+  function isAccessTokenExpired():Promise<boolean>;
+  function isAccessTokenExpiredSync():boolean;
+  function willAccessTokenExpireInLessThan():Promise<false|number>;
+  function willAccessTokenExpireInLessThanSync():false|number;
+}
+declare module 'Keycloak' {
+  function keycloakUILogin(conf:keycloakConfig, callback:Function, scope:scope):Promise<storedToken>;
+  function login(conf:keycloakConfig, username:string, password:string, options:loginOptions):Promise<JSON|Error>;
+  function refreshLogin(options:refreshLoginOptions):Promise<JSON|Error>;
+  function retrieveUserInfo():Promise<JSON|Error>;
+  function logout(conf: logoutInterface):Promise<void>;
+  function refreshToken(conf: refreshTokenInterface):Promise<JSON|Error>;
 }
