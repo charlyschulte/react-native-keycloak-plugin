3a657d004507d5c45cd09de53885dbca6f2bd346
charlyschulte <dddbernhard@yahoo.de>
2023-05-11 09:35:26 +0200

    test

--- src/Utils.d.ts
+++ src/Utils.d.ts
@@ -1,7 +1,14 @@
+import { keycloakConfig } from ".";
+
 declare const TokensUtils: {
     isAccessTokenExpired(): Promise<boolean>;
     isAccessTokenExpiredSync(): boolean;
     willAccessTokenExpireInLessThan(seconds:number): Promise<false | number>;
     willAccessTokenExpireInLessThanSync(seconds:number): false | number;
 }
-export {TokensUtils};
+declare function decodeToken(token:string):string;
+declare function getRealmURL(realm:string,authServerUrl:string):string;
+declare function getLoginURL(conf:keycloakConfig,scope:string):{url:string,state:string};
+export {TokensUtils,decodeToken,
+    getRealmURL,
+    getLoginURL};
