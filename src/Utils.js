42309146451412e65d7d647e46b4f9df051ab1b9
charlyschulte <dddbernhard@yahoo.de>
2023-05-25 12:34:12 +0200

    test

--- src/Utils.js
+++ src/Utils.js
@@ -1,5 +1,6 @@
 import { decode as atob } from 'base-64';
 import qs from 'query-string';
+import 'react-native-get-random-values'
 import { v4 as uuidv4 } from 'uuid';
 import TokenStorage from './TokenStorage';
 
